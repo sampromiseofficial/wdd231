1b2f498ae667a377ed7d7afa31416d2fbd84e27c
sampromiseofficial <sampromiseofficial@gmail.com>
2025-09-05 06:15:55 +0100

    G2G

---
+++ week1sub/kip/scripts/navigation.js
@@ -0,0 +1,9 @@
+//store the selected elements that we are going to use.
+const navbuttom = document.querySelector('#ham-btn');
+const navlinks = document.querySelector('nav-bar');
+
+//Toggle the show class off and on
+navbuttom.addEventListener('click', () => {
+    navbuttom.classList.toggle('show');
+    navlinks.classList.toggle('show');
+});
